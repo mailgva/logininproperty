595c0dc7857e989ad4ba2a96f7da00795030508a
Vladimir Gorbatenko <mail.gva@gmail.com>
2021-01-11 22:06:45 +0200

    first commit

---
+++ src/main/java/com/gorbatenko/logininproperty/LogininpropertyApplication.java
@@ -0,0 +1,13 @@
+package com.gorbatenko.logininproperty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogininpropertyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogininpropertyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gorbatenko/logininproperty/LogininpropertyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gorbatenko.logininproperty;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogininpropertyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
